d074a5200e1c69a49d094d23365d72608ce38504
pznikola <p.z.nikola@etf.rs>
2024-03-28 13:18:57 +0100

    Wrapper updated with AXI/AXIstream

---
+++ src/main/scala/DMAController/DMAWrapper.scala
@@ -0,0 +1,220 @@
+package DMAController
+
+import DMAController.CSR._
+import DMAController.DMAConfig.DMAConfig.{AXI, AXIL, AXIS}
+import DMAController.DMAConfig._
+import DMAController.Frontend._
+import DMAController.Worker.{InterruptBundle, SyncBundle, WorkerCSRWrapper}
+import DMAUtils._
+import chisel3._
+import freechips.rocketchip.amba.axi4._
+import freechips.rocketchip.amba.axi4stream._
+import org.chipsalliance.cde.config.Parameters
+import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp}
+
+// Read Node Parameters
+case class readNodeParams (
+  AXI4:       Option[Seq[AXI4SlavePortParameters]],
+  AXI4Stream: Option[AXI4StreamSlaveParameters]
+)
+
+// Write Node Parameters
+case class writeNodeParams (
+  AXI4:       Option[Seq[AXI4MasterPortParameters]],
+  AXI4Stream: Option[Seq[AXI4StreamMasterPortParameters]]
+)
+
+// Control Node Parameters
+case class controlNodeParams (
+  AXI4: Option[Seq[AXI4SlavePortParameters]]
+)
+
+// DMA Wrapper
+class DMAWrapper(
+  dmaConfig:     DMAConfig,
+  readerParams:  readNodeParams,
+  writerParams:  writeNodeParams,
+  controlParams: controlNodeParams
+) extends LazyModule()(Parameters.empty) {
+  val (reader, ccsr, writer) = dmaConfig.getBusConfig()
+
+  require(reader == AXI || reader == AXIS , "Reader must be either AXI or AXIS")
+  require(ccsr == AXIL, "Control must be AXI Lite")
+  require(writer == AXI || writer == AXIS , "Writer must be either AXI or AXIS")
+
+  val io_read    = if (reader == AXI) AXI4SlaveNode(readerParams.AXI4.get) else AXI4StreamSlaveNode(readerParams.AXI4Stream.get)
+  val io_write   = if (writer == AXI) AXI4MasterNode(writerParams.AXI4.get) else AXI4StreamMasterNode(writerParams.AXI4Stream.get)
+  val io_control = AXI4SlaveNode(controlParams.AXI4.get)
+
+  lazy val module = new LazyModuleImp(this) {
+
+    val (readNode,  _) = io_read.in.head
+    val (writeNode, _) = io_write.out.head
+    val (ctrlNode,  _) = io_control.in.head
+
+    val Bus = new Bus(dmaConfig)
+
+    val io = IO(new Bundle {
+      val irq = new InterruptBundle
+      val sync = new SyncBundle
+    })
+
+    val csrFrontend = Module(Bus.getCSR(ccsr))
+
+    val readerFrontend = Module(Bus.getReader(reader))
+
+    val writerFrontend = Module(Bus.getWriter(writer))
+
+    val csr = Module(new CSR(dmaConfig))
+
+    val ctl = Module(new WorkerCSRWrapper(dmaConfig))
+
+    val queue = DMAQueue(readerFrontend.io.dataIO, dmaConfig)
+    queue <> writerFrontend.io.dataIO
+
+    csr.io.bus <> csrFrontend.io.bus
+    ctl.io.csr <> csr.io.csr
+    readerFrontend.io.xfer <> ctl.io.xferRead
+    writerFrontend.io.xfer <> ctl.io.xferWrite
+
+    (readerFrontend.io.bus, readNode) match {
+      case (axis: DMAController.Bus.AXIStream, node_axis: AXI4StreamBundle) => {
+        axis.tdata           := node_axis.bits.data
+        axis.tvalid          := node_axis.valid
+        axis.tuser           := node_axis.bits.user
+        axis.tlast           := node_axis.bits.last
+        node_axis.ready      := axis.tready
+      }
+      case (axi: DMAController.Bus.AXI4, node_axi: AXI4Bundle) => {
+        // AW channel
+        node_axi.aw.bits.id    := axi.aw.awid
+        node_axi.aw.bits.addr  := axi.aw.awaddr
+        node_axi.aw.bits.len   := axi.aw.awlen
+        node_axi.aw.bits.size  := axi.aw.awsize
+        node_axi.aw.bits.burst := axi.aw.awburst
+        node_axi.aw.bits.lock  := axi.aw.awlock
+        node_axi.aw.bits.cache := axi.aw.awcache
+        node_axi.aw.bits.prot  := axi.aw.awprot
+        node_axi.aw.bits.qos   := axi.aw.awqos
+        node_axi.aw.valid      := axi.aw.awvalid
+        axi.aw.awready         := node_axi.aw.ready
+        // W channel
+        node_axi.w.bits.data   := axi.w.wdata
+        node_axi.w.bits.strb   := axi.w.wstrb
+        node_axi.w.bits.last   := axi.w.wlast
+        node_axi.w.valid       := axi.w.wvalid
+        axi.w.wready           := node_axi.w.ready
+        // B channel
+        axi.b.bid              := node_axi.b.bits.id
+        axi.b.bresp            := node_axi.b.bits.resp
+        axi.b.bvalid           := node_axi.b.valid
+        node_axi.b.ready       := axi.b.bready
+        // AR channel
+        node_axi.ar.bits.id    := axi.ar.arid
+        node_axi.ar.bits.addr  := axi.ar.araddr
+        node_axi.ar.bits.len   := axi.ar.arlen
+        node_axi.ar.bits.size  := axi.ar.arsize
+        node_axi.ar.bits.burst := axi.ar.arburst
+        node_axi.ar.bits.lock  := axi.ar.arlock
+        node_axi.ar.bits.cache := axi.ar.arcache
+        node_axi.ar.bits.prot  := axi.ar.arprot
+        node_axi.ar.bits.qos   := axi.ar.arqos
+        node_axi.ar.valid      := axi.ar.arvalid
+        axi.ar.arready         := node_axi.ar.ready
+        // R channel
+        axi.r.rid              := node_axi.r.bits.id
+        axi.r.rdata            := node_axi.r.bits.data
+        axi.r.rresp            := node_axi.r.bits.resp
+        axi.r.rlast            := node_axi.r.bits.last
+        axi.r.rvalid           := node_axi.r.valid
+        node_axi.r.ready       := axi.r.rready
+      }
+    }
+
+    (writerFrontend.io.bus, writeNode) match {
+      case (axis: DMAController.Bus.AXIStream, node_axis: AXI4StreamBundle) => {
+        node_axis.bits.data    := axis.tdata
+        node_axis.valid        := axis.tvalid
+        node_axis.bits.user    := axis.tuser
+        node_axis.bits.last    := axis.tlast
+        axis.tready            := node_axis.ready
+      }
+      case (axi: DMAController.Bus.AXI4, node_axi: AXI4Bundle) => {
+        // AW channel
+        node_axi.aw.bits.id    := axi.aw.awid
+        node_axi.aw.bits.addr  := axi.aw.awaddr
+        node_axi.aw.bits.len   := axi.aw.awlen
+        node_axi.aw.bits.size  := axi.aw.awsize
+        node_axi.aw.bits.burst := axi.aw.awburst
+        node_axi.aw.bits.lock  := axi.aw.awlock
+        node_axi.aw.bits.cache := axi.aw.awcache
+        node_axi.aw.bits.prot  := axi.aw.awprot
+        node_axi.aw.bits.qos   := axi.aw.awqos
+        node_axi.aw.valid      := axi.aw.awvalid
+        axi.aw.awready         := node_axi.aw.ready
+        // W channel
+        node_axi.w.bits.data   := axi.w.wdata
+        node_axi.w.bits.strb   := axi.w.wstrb
+        node_axi.w.bits.last   := axi.w.wlast
+        node_axi.w.valid       := axi.w.wvalid
+        axi.w.wready           := node_axi.w.ready
+        // B channel
+        axi.b.bid              := node_axi.b.bits.id
+        axi.b.bresp            := node_axi.b.bits.resp
+        axi.b.bvalid           := node_axi.b.valid
+        node_axi.b.ready       := axi.b.bready
+        // AR channel
+        node_axi.ar.bits.id    := axi.ar.arid
+        node_axi.ar.bits.addr  := axi.ar.araddr
+        node_axi.ar.bits.len   := axi.ar.arlen
+        node_axi.ar.bits.size  := axi.ar.arsize
+        node_axi.ar.bits.burst := axi.ar.arburst
+        node_axi.ar.bits.lock  := axi.ar.arlock
+        node_axi.ar.bits.cache := axi.ar.arcache
+        node_axi.ar.bits.prot  := axi.ar.arprot
+        node_axi.ar.bits.qos   := axi.ar.arqos
+        node_axi.ar.valid      := axi.ar.arvalid
+        axi.ar.arready         := node_axi.ar.ready
+        // R channel
+        axi.r.rid              := node_axi.r.bits.id
+        axi.r.rdata            := node_axi.r.bits.data
+        axi.r.rresp            := node_axi.r.bits.resp
+        axi.r.rlast            := node_axi.r.bits.last
+        axi.r.rvalid           := node_axi.r.valid
+        node_axi.r.ready       := axi.r.rready
+      }
+    }
+
+    (csrFrontend.io.ctl, ctrlNode) match {
+      case (axi: DMAController.Bus.AXI4Lite, node_axi: AXI4Bundle) => {
+        // AW channel
+        axi.aw.awaddr        := node_axi.aw.bits.addr
+        axi.aw.awprot        :=  node_axi.aw.bits.prot
+        axi.aw.awvalid       := node_axi.aw.valid
+        node_axi.aw.ready    := axi.aw.awready
+        // W channel
+        axi.w.wdata          := node_axi.w.bits.data
+        axi.w.wstrb          := node_axi.w.bits.strb
+        axi.w.wvalid         := node_axi.w.valid
+        node_axi.w.ready     := axi.w.wready
+        // B channel
+        node_axi.b.bits.resp := axi.b.bresp
+        node_axi.b.valid     := axi.b.bvalid
+        axi.b.bready         := node_axi.b.ready
+        // AR channel
+        axi.ar.araddr        := node_axi.ar.bits.addr
+        axi.ar.arprot        := node_axi.ar.bits.prot
+        axi.ar.arvalid       := node_axi.ar.valid
+        node_axi.ar.ready    := axi.ar.arready
+        // R channel
+        node_axi.r.bits.data := axi.r.rdata
+        node_axi.r.bits.resp := axi.r.rresp
+        node_axi.r.valid     := axi.r.rvalid
+        axi.r.rready         := node_axi.r.ready
+      }
+    }
+
+    io.irq <> ctl.io.irq
+    io.sync <> ctl.io.sync
+  }
+}
---
+++ src/main/scala/DMAController/DMAWrapperDriver.scala
@@ -0,0 +1,157 @@
+/*
+Copyright (C) 2019-2023 Antmicro
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package DMAController
+
+import DMAController.DMAConfig._
+import DMAUtils.{DMALogger, DMAParseInput}
+import chisel3.stage.ChiselStage
+import freechips.rocketchip.diplomacy.LazyModule
+import freechips.rocketchip.amba.axi4stream._
+import freechips.rocketchip.amba.axi4._
+import org.chipsalliance.cde.config.Parameters
+import freechips.rocketchip.diplomacy._
+
+object DMAWrapperDriver extends App {
+  // DMA configuration
+  val config = new DMAConfig(
+    busConfig        = "AXI_AXIL_AXI",
+    addrWidth        = 32,
+    readDataWidth    = 32,
+    writeDataWidth   = 32,
+    readMaxBurst     = 0,
+    writeMaxBurst    = 16,
+    reader4KBarrier  = false,
+    writer4KBarrier  = true,
+    controlDataWidth = 32,
+    controlAddrWidth = 32,
+    controlRegCount  = 16,
+    fifoDepth        = 512
+  )
+
+  // Read and Write address
+  private val readAddress  = AddressSet(0x4000, 0x0FFF)
+  private val writeAddress = AddressSet(0x0000, 0x0FFF)
+  private val ctrlAddress  = AddressSet(0x8000, 0x0FFF)
+
+  // Read node parameters
+  private val readParams = readNodeParams(
+    AXI4  = Some(Seq(AXI4SlavePortParameters(
+      Seq(AXI4SlaveParameters(
+        address       = Seq(readAddress),
+        supportsRead  = TransferSizes(1, config.readDataWidth/8),
+        supportsWrite = TransferSizes(1, config.readDataWidth/8),
+        interleavedId = Some(0))),
+      beatBytes = config.readDataWidth/8))),
+    AXI4Stream = Some(AXI4StreamSlaveParameters())
+  )
+
+  // Write node parameters
+  private val writeParams = writeNodeParams(
+    AXI4 = Some(Seq(AXI4MasterPortParameters(
+      Seq(AXI4MasterParameters(
+        name = "io_write",
+        id   = IdRange(0,15) ))))
+    ),
+    AXI4Stream = Some(Seq(AXI4StreamMasterPortParameters(
+      Seq(AXI4StreamMasterParameters(
+        name = "io_write",
+        n    = config.writeDataWidth/8 ))))
+    )
+  )
+
+  // Control node parameters
+  private val ctrlParams = controlNodeParams(
+    AXI4 = Some(Seq(AXI4SlavePortParameters(
+      Seq(AXI4SlaveParameters(
+        address       = Seq(ctrlAddress),
+        supportsRead  = TransferSizes(1, config.controlDataWidth / 8),
+        supportsWrite = TransferSizes(1, config.controlDataWidth / 8),
+        interleavedId = Some(0))),
+      beatBytes = config.controlDataWidth / 8)))
+  )
+
+  // Generate verilog
+  (new ChiselStage).emitVerilog(
+    args = Array(
+      "-X", "verilog",
+      "-e", "verilog",
+      "--target-dir", "verilog/LazyDMA"),
+    gen = LazyModule(new DMAWrapper(config, readParams, writeParams, ctrlParams){
+      io_control match {
+        case axi: AXI4SlaveNode => {
+          val ioInNode = BundleBridgeSource(() => AXI4Bundle(
+            AXI4BundleParameters(
+              addrBits       = config.controlAddrWidth,
+              dataBits       = config.controlDataWidth,
+              idBits         = 1,
+              echoFields     = Nil,
+              requestFields  = Nil,
+              responseFields = Nil
+            ))
+          )
+          axi := BundleBridgeToAXI4(AXI4MasterPortParameters(
+            Seq(AXI4MasterParameters(
+              name = "io_control",
+              id = IdRange(0, 0)
+            )))) := ioInNode
+          val io_control = InModuleBody { ioInNode.makeIO() }
+        }
+      }
+      io_read match {
+        case axis: AXI4StreamSlaveNode => {
+          val ioInNode = BundleBridgeSource(() => new AXI4StreamBundle(AXI4StreamBundleParameters(n = 4)))
+          axis := BundleBridgeToAXI4Stream(AXI4StreamMasterParameters(n = config.readDataWidth/8)) := ioInNode
+          val io_read = InModuleBody { ioInNode.makeIO() }
+        }
+        case axi: AXI4SlaveNode => {
+          val ioInNode = BundleBridgeSource(() => AXI4Bundle(
+              AXI4BundleParameters(
+                addrBits = config.addrWidth,
+                dataBits = config.readDataWidth,
+                idBits   = 4
+              ))
+          )
+          axi := BundleBridgeToAXI4(AXI4MasterPortParameters(
+            Seq(AXI4MasterParameters(
+              name = "io_read",
+              id = IdRange(0,15)
+            )))) := ioInNode
+          val io_read = InModuleBody { ioInNode.makeIO() }
+        }
+      }
+
+      io_write match {
+        case axis: AXI4StreamMasterNode => {
+          val ioOutNode = BundleBridgeSink[AXI4StreamBundle]()
+          ioOutNode := AXI4StreamToBundleBridge(AXI4StreamSlaveParameters()) := axis
+          val io_write = InModuleBody { ioOutNode.makeIO() }
+        }
+        case axi: AXI4MasterNode => {
+          val ioOutNode = BundleBridgeSink[AXI4Bundle]()
+          ioOutNode := AXI4ToBundleBridge(AXI4SlavePortParameters(
+            Seq(AXI4SlaveParameters(
+              address = Seq(writeAddress),
+              regionType = RegionType.UNCACHED,
+              executable = false,
+              supportsWrite = TransferSizes(1, config.writeDataWidth / 8),
+              supportsRead  = TransferSizes(1, config.writeDataWidth / 8)
+            )),
+            config.writeDataWidth/8)) := axi
+          val io_write = InModuleBody { ioOutNode.makeIO() }
+        }
+      }
+    }).module
+  )
+}
